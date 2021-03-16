e671bcf446298f959009a468d2c9fce287bbf470
Lucas Macedo <lucasmdc57@gmail.com>
2021-03-16 17:03:11 -0300

    adding transaction type button behavior

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
 import Modal from 'react-modal';
-import { Container, TransactionTypeContainer } from './styles'
+import { Container, TransactionTypeContainer, RadioBox } from './styles'
 
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
+import { useState } from 'react';
 
 Modal.setAppElement('#root')
 
@@ -13,6 +14,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit')
+
   return (
     <Modal
       isOpen={isOpen}
@@ -41,15 +44,23 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         />
 
         <TransactionTypeContainer>
-          <button type="button">
+          <RadioBox
+            type="button"
+            onClick={() => setType('deposit')}
+            isActive={type === 'deposit'}
+          >
             <img src={incomeImg} alt="Entrada"/>
             <span>Entrada</span>
-          </button> 
+          </RadioBox> 
           
-          <button type="button">
+          <RadioBox
+            type="button"
+            onClick={() => setType('withdraw')}
+            isActive={type === 'withdraw'}
+          >
             <img src={outcomeImg} alt="Saída"/>
             <span>Saída</span>
-          </button> 
+          </RadioBox> 
         </TransactionTypeContainer>
 
         <input 
