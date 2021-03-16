3db18b6120af2d152001322ec8acfa91d09d318d
Lucas Macedo <lucasmdc57@gmail.com>
2021-03-16 17:32:29 -0300

    implements transactions fake api with mirage

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          amount: 6000,
+          createdAt: new Date('2021-03-16 17:22:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-03-16 18:22:00')
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
-    this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-        }
-      ]
+    this.get('/transactions', () => this.schema.all('transaction'))
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
     })
   }
 })
