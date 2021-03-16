ec5849c10445e048d077fa781824d011bdd43fcc
Lucas Macedo <lucasmdc57@gmail.com>
2021-03-15 21:43:19 -0300

    adding global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+
+      <GlobalStyle />
+    </>
   );
 }
